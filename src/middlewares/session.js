7d2482a608f96929980548ef950e801d0361b0b6
mathieuz <matheusandrewww@gmail.com>
2024-01-14 17:38:12 -0300

    Listando categorias registradas.

--- src/middlewares/session.js
+++ src/middlewares/session.js
@@ -3,7 +3,8 @@ const session = require("express-session")
 module.exports = function() {
     return session({
         secret: "protonodejs7secret",
-        resave: true,
-        saveUninitialized: true
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
     })
 }
